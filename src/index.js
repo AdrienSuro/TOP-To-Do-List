becc89dc5409e4d823257401f551080945fc9b7b
AdrienSuro <adrien.surowiec@protonmail.com>
2023-02-05 17:17:43 +0100

    Dirty brainstorming

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 console.log("It works");
 
-const createTask = (title, description, dueDate, priority) => {
-  const sayHello = () => console.log(`Hello ${title}`);
-  return {title, description, dueDate, priority, sayHello};
+const createTask = (title) => {
+  const description = "Enter a description";
+  const dueDate = "January";
+  const priority = "high";
+
+  return {title, description, dueDate, priority}
 };
 
-const createToDo = (title) => {
-  let taskArray = new Array();
-  // let addTaskToArray = () => taskArray.push(let newTask = createTask(""))
-  return {title}
+const createToDoList = (title) => {
+  const remove = () => toDoArray.slice;
+  const move = () => ;
+  const tasks = new Object();
+  let i = 1;
+  const addTask = () => {
+    let taskTitle = prompt("Enter title")
+    i+=1;
+
+
+  return {title, tasks, remove, move, addTask}
 }
 
+//List of the different To-Dos : 
+
 const toDoArray = new Array();
+
+const addListBtn = document.getElementById("addListBtn");
+addListBtn.addEventListener("click", addListToToDoArray)
+
+function addListToToDoArray() {
+  let newList = createToDoList(....); //update the arguments
+  toDoArray.push(newList);
+}
 
 
 
 // Créer une fonction qui permet de réordonner les ToDo selon leur degré d'importance. Cette fonction agira sur l'array de ToDo.
+const personFactory = (name, age) => {
+  const sayHello = () => console.log('hello!');
+  return { name, age, sayHello };
+};
+
+const jeff = personFactory('jeff', 27);
+
+console.log(jeff.name); // 'jeff'
+
+jeff.sayHello(); // calls the function and logs 'hello!'
+
+let testList = ["string", 5, {title: "Work", importance: "high"}]
+console.log(testList[2]);
+
+let testObject = {
+  firstTask: {title: "Work", importance:"high"}, 
+  secondTask: {title: "Fun", importance: "low"},
+  thirdTask : {title: "Family", importance: "medium"}}
+
+console.log(testObject.firstTask.title);
