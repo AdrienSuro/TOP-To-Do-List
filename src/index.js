223673d55886c11c642d90f065768a1bd94b8190
AdrienSuro <adrien.surowiec@protonmail.com>
2023-02-07 06:44:36 +0100

    Create array of tasks to access them easily

--- src/index.js
+++ src/index.js
@@ -1,47 +1,37 @@
 import createBlock from "./init.js";
 
-console.log("js works fine");
-console.log(this === undefined ? "true" : "false");
-
 createBlock();
 
-const createTask = (title) => {
-  const test = "we are inside a task"
-  const testlog = console.log("Testlog inside the task works")
-  return {title, test}
+function createTask(title) {
+  const test = `we are inside the ${title} task`;
+  const testlog = function() {console.log("Testlog inside the task works")};
+  return {title, testlog, test}
 }
 
 const createToDoList = (title) => {
   const toDoArray = [];
   const remove = () => toDoArray.slice;
   const move = () => toDoArray.splice;
-  const tasks = new Object();
+  const taskArray = new Array();
   const addTask = function() {
     console.log(this);
     let promptTaskName = prompt("What's the task name?");
     this[promptTaskName] = createTask(promptTaskName);
+    taskArray.push(promptTaskName);
   }
 
-  return {remove, move, tasks, addTask, title };
+  return {remove, move, taskArray, addTask, title };
 };
 
 const firstToDoList = createToDoList("Work");
-console.log(firstToDoList);
-console.log("line21");
 firstToDoList.addTask();
 firstToDoList.addTask();
+let z = firstToDoList.taskArray.length;
 
+for (let i = z-1; i>=0; i--) {
+  console.log(firstToDoList.taskArray[i]);
+};
 
-console.log(firstToDoList.js);
-console.log(firstToDoList.html);
-
-firstToDoList.js.testlog;
-
-
-
-
-
-
-
+firstToDoList.js.testlog();
 
 console.log("Reached the end of index.js")
